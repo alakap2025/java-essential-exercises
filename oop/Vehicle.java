581b6d8a8d81126d045c402e84785cd8f4928e73
alakap2025 <alakap2025@gmail.com>
2026-01-07 13:12:13 +0000

    Implement Vehicle, Car, and ElectricCar classes

--- oop/Vehicle.java
+++ oop/Vehicle.java
@@ -1,1 +1,53 @@
+/*
+ * Click `Run` to execute the snippet below!
+ */
+
+import java.io.*;
+import java.util.*;
+
+/*
+ * To execute Java, please define "static void main" on a class
+ * named Solution.
+ *
+ * If you need more classes, simply define them inline.
+ */
+
+class Vehicle
+{
+  void start()
+  {
+    System.out.print("Vehicle starting ...");
+  }
+  
+}
+
+class Car extends Vehicle
+{
+  @Override
+  void start()
+  {
+    System.out.print("Car engine starting...");
+  }
+}
+
+class ElectricCar extends Car{
+  @Override
+  void start()
+  {
+    System.out.println("Electric car powering on silently...");
+  }
+
+   public static void main(String args[])
+  {
+    Vehicle v = new Vehicle();
+    Vehicle c = new Car();
+    Vehicle e = new ElectricCar();
+    
+    v.start();
+    c.start();
+    e.start();
+
+  }
+
+}
 
